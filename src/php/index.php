4554b8f0a6f9ad3a9c990cd7eb7fdb1b121cfa00
Kirill Neevin <neevin-kirill@mail.ru>
2021-09-09 14:45:07 +0300

    Поправиал баг, начинаю делать доп. задание

--- src/php/index.php
+++ src/php/index.php
@@ -1,22 +1,6 @@
 <?php
 $start = microtime(true); // Время начала работы скрипта
 
-echo '
-    <!DOCTYPE html>
-    <html lang="ru">
-        <head>
-            <meta charset="utf-8">
-            <meta name="author" content="Kirill Neevin">
-            <title>Неевин Кирилл</title>
-            <link rel="icon" href="../img/icon.ico">
-            <link rel="stylesheet" href="../style.css">
-        </head>
-        <body>
-            <header id="header">
-                Неевин Кирилл P3213, выриант: 13201
-            </header>
-';
-
 function check_hit($x, $y, $r): bool
 {
     if($x >= 0 and $y >= 0){ // 1
@@ -47,36 +31,61 @@ function validate($x, $y, $r): bool
     return -3 <= $x and $x <= 5 and -5 <= $y and $y <= 5 and 1 <= $r and $r <= 4;
 }
 
-$x = floatval($_GET['x']);
-$y = floatval($_GET['y']);
-$r = floatval($_GET['r']);
+// Парсинг аргументов GET-запроса
 
-if(!validate($x, $y, $r)){
-    echo "Неверные поля GET запроса. Измените и отправьте снова.";
-    exit();
+$x = ($_GET['x']);
+$y = ($_GET['y']);
+$r = ($_GET['r']);
+
+if(isset($x) and isset($y) and isset($r)){
+    global $x, $y, $r;
+
+    $y = str_replace(',','.',$y);
+    $r = str_replace(',','.',$r);
+
+    $x = (float)$x;
+    $y = (float)$y;
+    $r = (float)$r;
 }
 
-$is_hit = check_hit($x, $y, $r);
+if(validate($x, $y, $r)){
+    //
+    $is_hit = check_hit($x, $y, $r);
 
-$curr = array(
-    "x" => $x,
-    "y" => $y,
-    "r" => $r,
-    "is_hit" => $is_hit
-);
+    $curr = array(
+        "x" => $x,
+        "y" => $y,
+        "r" => $r,
+        "is_hit" => $is_hit
+    );
 
-session_start();
-if (isset($_SESSION['saves'])) {
-    $saves = unserialize($_SESSION['saves']);
-    array_push($saves, $curr);
-    $_SESSION['saves'] = serialize($saves);
-} else {
-    $saves = array();
-    array_push($saves, $curr);
-    $_SESSION['saves'] = serialize($saves);
-}
+    session_start();
+    if (isset($_SESSION['saves'])) {
+        $saves = unserialize($_SESSION['saves']);
+        array_push($saves, $curr);
+        $_SESSION['saves'] = serialize($saves);
+    } else {
+        $saves = array();
+        array_push($saves, $curr);
+        $_SESSION['saves'] = serialize($saves);
+    }
 
-echo "<div class='center-wrapper'>
+    echo '
+    <!DOCTYPE html>
+    <html lang="ru">
+        <head>
+            <meta charset="utf-8">
+            <meta name="author" content="Kirill Neevin">
+            <title>Неевин Кирилл</title>
+            <link rel="icon" href="../img/icon.ico">
+            <link rel="stylesheet" href="../style.css">
+        </head>
+        <body>
+            <header id="header">
+                Неевин Кирилл P3213, выриант: 13201
+            </header>';
+
+    echo "<div class='center-wrapper'>
     <table class='inner'>
         <tr>
             <th>X</th>
@@ -85,28 +94,33 @@ echo "<div class='center-wrapper'>
             <th>Есть попадание</th>
         </tr>
 ";
-foreach ($saves as &$element) {
-    echo "
+    foreach ($saves as &$element) {
+        echo "
         <tr>
             <td>".$element["x"]."</td>
             <td>".$element["y"]."</td>
             <td>".$element["r"]."</td>
             <td>".($element["is_hit"] ? "да" : "нет")."</td>
         </tr>";
-}
-echo "</table></div>";
+    }
+    echo "</table></div>";
 
-echo '<p id="info-pannel">';
-echo "Текущая дата: " . date("d M Y, H:i:s") . "<br>";
-echo "Время работы скрипта: " . (microtime(true) - $start) . " мс";
-echo '</p>';
+    echo '<p id="info-pannel">';
+    echo "Текущая дата: " . date("d M Y, H:i:s") . "<br>";
+    echo "Время работы скрипта: " . (microtime(true) - $start) . " мс";
+    echo '</p>';
 
-echo '
+    echo '
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="../script.js"></script>
     </body>
 </html>
 ';
+
+}
+else{
+    echo "Неверные поля GET запроса. Измените и отправьте снова.";
+}
 
 // Очистить данные сессии
 $reset = $_GET['reset'] ?? 'no';
